46b21f5b12f0c67292cee737797d99140077280b
Daniel <dani.mbirochi@gmail.com>
2023-02-21 17:13:22 -0300

    Implement automated test for quicksort

---
+++ quicksort/quicksort_test.go
@@ -0,0 +1,27 @@
+package quicksort_test
+
+import (
+	"fmt"
+	"sort"
+	"testing"
+
+	"github.com/danielmbirochi/Golang-Algorithms-And-Data-Structures/quicksort"
+)
+
+func TestQuicksort(t *testing.T) {
+
+	t.Run("Sorting using LomutoQuicksort", func(t *testing.T) {
+		input := []int{2, 1, 5, 7, 3, 8, 4, 9, 0, 6}
+		v := quicksort.NewVector(len(input))
+		copy(*v, input)
+		fmt.Printf("Input : %v\n", *v)
+
+		quicksort.Quicksort(v, 0, len(*v)-1)
+		fmt.Printf("Output : %v\n", *v)
+
+		if sort.IsSorted(v) {
+			t.Log("Slice sorted w/ Quicksort successfully!")
+		}
+	})
+
+}
